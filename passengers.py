79e89448d256767cf7d6b35e448469cf8e5b7da4
Robert Clark <robdclark@outlook.com>
2017-11-11 11:44:34 -0600

    Remove unused Passenger class

--- passengers.py
+++ passengers.py
@@ -2,88 +2,20 @@ import csv
 import pandas
 
 
+HEADER = ['PassengerId', 'Survived', 'Pclass', 'Name', 'Sex', 'Age', 'SibSp',
+          'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked']
 GENDER_PARSE = {'male': 0, 'female': 1}
 PORT_OF_EMBARKMENT = {'Q': 0, 'S': 1, 'C': 2, '': -1}
 
 
-class Passenger:
-    def __init__(self, passengerid, survived, pclass, name, sex, age, sibsp,
-                 parch, ticket, fare, cabin, embarked):
-        self._passengerid = passengerid
-        self._survived = survived
-        self._pclass = pclass
-        self._name = name
-        self._sex = GENDER_PARSE[sex]
-        self._age = age
-        self._sibsp = sibsp
-        self._parch = parch
-        self._ticket = ticket
-        self._fare = fare
-        self._cabin = cabin
-        self._embarked = PORT_OF_EMBARKMENT[embarked]
-
-    @property
-    def passengerid(self):
-        return self._passengerid
-
-    @property
-    def survived(self):
-        return self._survived
-
-    @property
-    def pclass(self):
-        return self._pclass
-
-    @property
-    def name(self):
-        return self._name
-
-    @property
-    def sex(self):
-        return self._sex
-
-    @property
-    def age(self):
-        return self._age
-
-    @property
-    def sibsp(self):
-        return self._sibsp
-
-    @property
-    def parch(self):
-        return self._parch
-
-    @property
-    def ticket(self):
-        return self._ticket
-
-    @property
-    def fare(self):
-        return self._fare
-
-    @property
-    def cabin(self):
-        return self._cabin
-
-    @property
-    def embarked(self):
-        return self._embarked
-
-
 class Passengers:
     def __init__(self, input_file):
-        self._passengers = []
-        self._header = []
+        self._header = HEADER
         self._feature_sets = {}
 
         passenger_data = pandas.read_csv(input_file, header=0)
         passenger_data = self._edit_passenger_data(passenger_data)
-        self._read_input_file(input_file)
         self._build_feature_sets(passenger_data)
-
-    def __iter__(self):
-        return iter(self._passengers)
 
     @property
     def header(self):
@@ -100,13 +32,6 @@ class Passengers:
             data = data.replace(port, PORT_OF_EMBARKMENT[port])
         data = data.where((pandas.notnull(data)), None)
         return data
-
-    def _read_input_file(self, input_file):
-        with open(input_file, 'rb') as passenger_list:
-            reader = csv.reader(passenger_list)
-            self._header = next(reader, None)  # Save the header line
-            for passenger in reader:
-                self._passengers.append(Passenger(*passenger))
 
     def _build_feature_sets(self, data):
         for header in self._header:
--- tests/test_passengers.py
+++ tests/test_passengers.py
@@ -1,62 +1,28 @@
-from passengers import Passenger, Passengers
+from collections import Counter
+from passengers import Passengers
 
 
 FEATURE_SETS = {
-    'Fare': list(set([7.25, 71.2833, 7.925])),
-    'Name': list(set([
+    'Fare': [7.25, 71.2833, 7.925],
+    'Name': [
              'Braund, Mr. Owen Harris',
              'Cumings, Mrs. John Bradley (Florence Briggs Thayer)',
              'Heikkinen, Miss. Laina'
-            ])),
-    'Embarked': list(set([1, 2])),
-    'Age': list(set([22, 38, 26])),
-    'Parch': list(set([0])),
-    'Pclass': list(set([1, 2, 3])),
-    'Sex': list(set([0, 1])),
-    'Survived': list(set([0, 1])),
-    'SibSp': list(set([1, 0])),
-    'PassengerId': list(set([1, 2, 3])),
-    'Ticket': list(set(['A/5 21171', 'PC 17599', 'STON/O2. 3101282'])),
-    'Cabin': list(set([None, 'C85']))
+            ],
+    'Embarked': [1, 2],
+    'Age': [22, 38, 26],
+    'Parch': [0],
+    'Pclass': [1, 2, 3],
+    'Sex': [0, 1],
+    'Survived': [0, 1],
+    'SibSp': [1, 0],
+    'PassengerId': [1, 2, 3],
+    'Ticket': ['A/5 21171', 'PC 17599', 'STON/O2. 3101282'],
+    'Cabin': [None, 'C85']
 }
-GENDER_PARSE = {'male': 0, 'female': 1}
 HEADERS = ['PassengerId', 'Survived', 'Pclass', 'Name', 'Sex', 'Age', 'SibSp',
            'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked']
-NUM_PASSENGERS = 3
-PORT_OF_EMBARKMENT = {'Q': 0, 'S': 1, 'C': 2, '': -1}
 SAMPLE_DATA = 'tests/sample_training.csv'
-
-
-class TestPassenger:
-    def test_passenger_creation(self):
-        passengerid = 810
-        survived = 1
-        pclass = 2
-        name = "John Doe"
-        sex = "male"
-        age = 28
-        sibsp = 0
-        parch = 0
-        ticket = "148625"
-        fare = 8.05
-        cabin = ""
-        embarked = "S"
-
-        passenger = Passenger(passengerid, survived, pclass, name, sex, age,
-                              sibsp, parch, ticket, fare, cabin, embarked)
-
-        assert passenger.passengerid == passengerid
-        assert passenger.survived == survived
-        assert passenger.pclass == pclass
-        assert passenger.name == name
-        assert passenger.sex == GENDER_PARSE[sex]
-        assert passenger.age == age
-        assert passenger.sibsp == sibsp
-        assert passenger.parch == parch
-        assert passenger.ticket == ticket
-        assert passenger.fare == fare
-        assert passenger.cabin == cabin
-        assert passenger.embarked == PORT_OF_EMBARKMENT[embarked]
 
 
 class TestPassengers:
@@ -64,9 +30,6 @@ class TestPassengers:
         passengers = Passengers(SAMPLE_DATA)
 
         assert passengers.header == HEADERS
-        assert passengers.feature_sets == FEATURE_SETS
-
-        num_passengers = 0
-        for passenger in passengers:
-            num_passengers += 1
-        assert num_passengers == NUM_PASSENGERS
+        for feature, value in passengers.feature_sets.items():
+            assert Counter(FEATURE_SETS[feature]) == Counter(value)
+        assert len(passengers.feature_sets) == len(FEATURE_SETS)
